5434848006a67e5bffb2c6de1340a553f908fa80
gMel22 <86202697+gMel22@users.noreply.github.com>
2021-06-20 21:00:22 +0300

    Create wwdu.m

---
+++ wwdu.m
@@ -0,0 +1,28 @@
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack0]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack1]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack2]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack3]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack4]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack5]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack6]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack7]
+"Installed"="True"
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 4\Xpack8]
+"Installed"="True"
